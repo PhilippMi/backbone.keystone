f2d89d3d129fbb7790b39704fd8d3ae890ed1ea3
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2015-09-17 11:04:02 +0200

    Add JSPM and create new index page

---
+++ public/config.js
@@ -0,0 +1,32 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "babel": "npm:babel@4.7.16",
+    "babel-runtime": "npm:babel-runtime@4.7.16",
+    "core-js": "npm:core-js@1.1.4",
+    "marionette": "github:marionettejs/backbone.marionette@2.4.3",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@4.7.16": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.4": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+document.getElementById("page-main").innerHTML = '<h2>it worked!</h2><p>Hello, World!</p>';
