d3f9740b2700145b6463c07843dfc490d64a262c
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2015-09-17 10:29:58 +0200

    Add API for posts

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,8 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views')
+	views: importRoutes('./views'),
+	api: importRoutes('./api')
 };
 
 // Setup Route Bindings
@@ -40,6 +41,12 @@ exports = module.exports = function(app) {
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
+
+	app.get('/api/post/list', keystone.middleware.api, routes.api.posts.list);
+	app.all('/api/post/create', keystone.middleware.api, routes.api.posts.create);
+	app.get('/api/post/:id', keystone.middleware.api, routes.api.posts.get);
+	app.all('/api/post/:id/update', keystone.middleware.api, routes.api.posts.update);
+	app.get('/api/post/:id/remove', keystone.middleware.api, routes.api.posts.remove);
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
