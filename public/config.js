eaa8203f842aeeb52b8b7784312390b1d64a7f32
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2015-09-17 13:49:39 +0200

    Show posts as Backbone/Marionette view/model
    This already uses the real data stored in the DB.

--- public/config.js
+++ public/config.js
@@ -15,13 +15,22 @@ System.config({
   map: {
     "babel": "npm:babel@4.7.16",
     "babel-runtime": "npm:babel-runtime@4.7.16",
+    "backbone": "npm:backbone@1.2.3",
+    "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.10",
+    "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.5",
     "core-js": "npm:core-js@1.1.4",
+    "jquery": "github:components/jquery@2.1.4",
     "marionette": "github:marionettejs/backbone.marionette@2.4.3",
+    "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@4.7.16": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:backbone@1.2.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "underscore": "npm:underscore@1.8.3"
     },
     "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
-document.getElementById("page-main").innerHTML = '<h2>it worked!</h2><p>Hello, World!</p>';
+import Backbone from 'backbone';
+import Marionette from 'marionette';
+
+let PostCollection = Backbone.Collection.extend({
+	url: '/api/post/list',
+	parse(attributes) {
+		return attributes.posts;
+	}
+});
+
+let collection = new PostCollection();
+
+let PostView = Marionette.ItemView.extend({
+	
+	template(p) {
+		return '<article><h2>' + p.title + '</h2>' +
+				'<p>' + p.content.brief + '</p></article>';
+	}
+	
+});
+
+let PostsView = Marionette.CollectionView.extend({
+	childView: PostView 
+});
+
+let view = new PostsView({collection: collection});
+let region = new Marionette.Region({el: '#page-main'});
+region.show(view);
+
+collection.fetch();
+
